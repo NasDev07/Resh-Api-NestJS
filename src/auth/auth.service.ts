aae07030a566317cdf48db2ecec35c628bb3119a
NasDev07 <nasruddin20182019@gmail.com>
2023-11-22 10:05:58 +0700

    Login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private useService: UserService) {}
+  async login(dto: AuthDto) {
+    const user = await this.useService.findByEmail(dto.email);
+    const { password, ...result } = user;
+    if (user) {
+      return result;
+    }
+
+    throw new UnauthorizedException();
+  }
+}
