a1bc811363800259200d01ad72f5d9bd2c942f57
NasDev07 <nasruddin20182019@gmail.com>
2023-11-22 10:13:00 +0700

    verifikasi email dan password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,15 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
 import { UserService } from 'src/user/user.service';
+import { compare } from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
   constructor(private useService: UserService) {}
   async login(dto: AuthDto) {
     const user = await this.useService.findByEmail(dto.email);
-    const { password, ...result } = user;
-    if (user) {
+    if (user && (await compare(dto.password, user.password))) {
+      const { password, ...result } = user;
       return result;
     }
 
